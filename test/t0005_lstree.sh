ecf00e3bfd11bbc1af679c1de6e28f15efd47790
Victor Boivie <victor-dev@boivie.com>
2010-03-20 23:22:16 +0100

    Added test case for lstree

---
+++ test/t0005_lstree.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+test_description='Test indexing'
+
+. ./test-lib.sh
+
+cat >tree.xml <<EOF
+<tree>
+ <treeref name="subdir_one" mode="040755" mtime="123457890" ctime="1234567890" sha="b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c" />
+ <treeref name="subdir_two" mode="040755" mtime="123457891" ctime="1234567892" sha="7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730" />
+</tree>
+EOF
+
+cat >expected <<EOF
+References:
+       0: 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730
+       1: b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c
+
+Contents:
+drwxr-xr-x b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c subdir_one
+drwxr-xr-x 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730 subdir_two
+EOF
+
+test_expect_success 'listing simple tree' '
+  (
+    lyekka mktree < tree.xml > actual &&
+    lyekka lstree -i a548d2e141f8759f53a29207fbbb2d4a5471ef54bfd88fd406896559b5071c86 > actual &&
+    test_cmp expected actual 
+  )'
+
+
+cat >tree2.xml <<EOF
+<tree>
+ <treeref name="subdir_one" mode="040755" mtime="123457890" ctime="1234567890" sha="b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c"/>
+ <treeref name="subdir_two" mode="040755" mtime="123457891" ctime="1234567892" sha="7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730"/>
+ <fileref name="file1" mode="100644" mtime="0987654321" ctime="0987654322" size="1020304050">
+  <part offset="0" size="10000" sha="30520052695c48c1bae453774d29c2c1f9fdbf10087b16d96a367112c2c482ef"/>
+  <part offset="10000" size="10000" sha="abb4d6b4a7568591ce172590bb6d96f8968d86425761fbc148685a9ad0c4c644"/>
+ </fileref>
+</tree>
+EOF
+
+cat >expected <<EOF
+References:
+       0: 30520052695c48c1bae453774d29c2c1f9fdbf10087b16d96a367112c2c482ef
+       1: 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730
+       2: abb4d6b4a7568591ce172590bb6d96f8968d86425761fbc148685a9ad0c4c644
+       3: b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c
+
+Contents:
+drwxr-xr-x b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c subdir_one
+drwxr-xr-x 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730 subdir_two
+-rw-r--r-- ---------------------------------------------------------------- file1
+---------- 30520052695c48c1bae453774d29c2c1f9fdbf10087b16d96a367112c2c482ef [0--10000]
+---------- abb4d6b4a7568591ce172590bb6d96f8968d86425761fbc148685a9ad0c4c644 [10000--20000]
+EOF
+
+test_expect_success 'listing more advanced tree' '
+  (
+    lyekka mktree < tree2.xml > actual &&
+    lyekka lstree -i 0b11700c7c58fb8ca5b57b4273bfd1c3e4c48646f77280f6469f28b749b3f2ab > actual &&
+    test_cmp expected actual 
+  )'
+
+test_done
